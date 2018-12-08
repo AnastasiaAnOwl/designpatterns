1a24db5b054cc9ec0d3ba972e1ecaf782df97986
AnOwl <a.v.kunatava@gmail.com>
2018-12-08 19:16:29 +0300

    Mallard duck added

---
+++ src/by/owl/patterns/strategy/MallardDuck.java
@@ -0,0 +1,25 @@
+package by.owl.patterns.strategy;
+
+public class MallardDuck extends Duck {
+
+	@Override
+	public void quack() {
+		System.out.println("Quack!");
+		
+	}
+
+	@Override
+	public void swim() {
+		System.out.println("swim-swim");
+		
+	}
+
+	@Override
+	public void display() {
+		System.out.println("I'm a mallard duck! ");
+		
+	}
+	
+	
+
+}
