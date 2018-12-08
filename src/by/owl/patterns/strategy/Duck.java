0401e32a6ba7a9b490959a587a6377a2a9201304
AnOwl <a.v.kunatava@gmail.com>
2018-12-08 17:37:36 +0300

    javadoc added

--- src/by/owl/patterns/strategy/Duck.java
+++ src/by/owl/patterns/strategy/Duck.java
@@ -1,13 +1,15 @@
 package by.owl.patterns.strategy;
 
+/**
+ * Parent class for all ducks
+ * 
+ * @author anowl
+ *
+ */
 public abstract class Duck {
 	
-
 	public abstract void quack();
 	public abstract void swim();
 	public abstract void display();
-	
-
-	
 
 }
